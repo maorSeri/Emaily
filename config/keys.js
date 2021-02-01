1163de065edfce61d80460578eaea8ce00dca98f
97252 <maorroqm14@gmail.com>
2021-02-01 13:19:14 +0200

    fix

--- config/keys.js
+++ config/keys.js
@@ -1,8 +1,8 @@
 if(process.env.NODE_ENV === 'production') {
     // return the prod set of keys
-    module.exports = require('prod')
+    module.exports = require('./prod')
 } else {
     //return dev keys
-    module.exports = require('dev');
+    module.exports = require('./dev');
 }
 
